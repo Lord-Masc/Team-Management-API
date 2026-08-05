2cef405a7492e24d080c7e763cdc05d5cf5f1c2d
Yash verma <yashverma9084+github@gmail.com>
2026-08-05 18:56:54 +0530

    Result validation

---
+++ src/middlewares/validate.js
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+
+const validate = (req,res,next)=>{
+    const error = validationResult(req)
+
+    if(error.isEmpty()) return next()
+
+    return res.status(400).json({
+        sucess:false,
+        message:"validation faild",
+        errors:errors.array()
+    })
+}
